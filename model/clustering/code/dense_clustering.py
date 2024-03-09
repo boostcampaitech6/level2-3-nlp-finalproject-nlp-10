4ab25f469005bc8a73f61dd73ea62d79f5ba894c
EbanLee <rudwo6769@gmail.com>
2024-03-10 01:16:36 +0900

    fix summarizing #21

--- model/clustering/code/dense_clustering.py
+++ model/clustering/code/dense_clustering.py
@@ -3,42 +3,55 @@ import os
 import hdbscan
 from collections import Counter
 import time
+import argparse
 
 dataset_path = '../dataset'
-dataset = pd.read_csv(os.path.join(dataset_path, "add_embedding.csv"))
-
-times = dataset['datetime'].unique()
-
+dataset = pd.read_csv(os.path.join(dataset_path, "add_embedding_Sum_FIN_NEWS_SUMMARY.csv"))
 embeddings = list(map(lambda x : list(map(lambda y : float(y), x[1:-1].split(','))), dataset['embedding'].tolist()))
 
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("--make", default='False', type=str, help='make topic? False = read')
+args = parser.parse_args()
 
-# HDBSCAN 실행
-def hdbscan_process(corpus, corpus_embeddings, min_cluster_size=3, min_samples=3, method='eom'):
-    cluster = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size,
-                              min_samples=min_samples,
-                              metric='euclidean',
-                              allow_single_cluster=True,
-                              cluster_selection_method=method,
-                              ).fit(corpus_embeddings) #eom leaf
+if args.make == 'True':
+    print("make_Topic")
+    # HDBSCAN 실행
+    def hdbscan_process(corpus, corpus_embeddings, min_cluster_size=3, min_samples=3, method='eom'):
+        cluster = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size,
+                                min_samples=min_samples,
+                                metric='euclidean',
+                                allow_single_cluster=True,
+                                cluster_selection_method=method,
+                                ).fit(corpus_embeddings) #eom leaf
 
-    docs_df = corpus
-    docs_df['Topic'] = cluster.labels_
-    #print(Counter(cluster.labels_), '\n')
-    
-    return docs_df, Counter(cluster.labels_)
+        docs_df = corpus
+        docs_df['Topic'] = cluster.labels_
+        #print(Counter(cluster.labels_), '\n')
+        
+        return docs_df, Counter(cluster.labels_)
 
-start_time = time.time()
-docs_df, cluster_labels = hdbscan_process(dataset, 
-                          embeddings,
-                          method='leaf',    #가장 높은 밀도
-                          min_cluster_size=3,
-                          min_samples=3,
-                          )
+    start_time = time.time()
+    docs_df, cluster_labels = hdbscan_process(dataset, 
+                            embeddings,
+                            method='leaf',    #가장 높은 밀도
+                            min_cluster_size=3,
+                            min_samples=3,
+                            )
 
-elapsed_time = time.time() - start_time
+    elapsed_time = time.time() - start_time
+    print(f'duration : {elapsed_time}s\n')
+    docs_df.to_csv(os.path.join(dataset_path,'add_Topic.csv'))
+
+else:
+    print("read_TopicData")
+    docs_df = pd.read_csv(os.path.join(dataset_path, 'add_Topic.csv'))
+    cluster_labels = Counter(docs_df['Topic'].to_list())
+    embeddings = list(map(lambda x : list(map(lambda y : float(y), x[1:-1].split(','))), docs_df['embedding'].tolist()))
+
+times = docs_df['datetime'].unique()
+
 print(f'\nstart ~ end : {times[0]} ~ {times[-1]}')
 print(f'num_docs : {len(embeddings)}')
-print(f'duration : {elapsed_time}s\n')
 
 def check_numeric(input_str):
     try:
@@ -51,7 +64,12 @@ while(True):
     t_num = input(f'{cluster_labels}\nTopic_num (정수 아닌 입력 시 종료): ')
     if check_numeric(t_num):
         for idx in range(len(docs_df)):
-            print(f"\n{docs_df[docs_df['Topic']==int(t_num)]['datetime'].iloc[idx]}")
-            print(f"{docs_df[docs_df['Topic']==int(t_num)]['summary'].iloc[idx]}")
+            if docs_df['Topic'].iloc[idx] == int(t_num):
+                print(f"{idx}: {docs_df['datetime'].iloc[idx]}")
+                print(f"{docs_df['summary'].iloc[idx]}\n")
+
+        # for idx in range(len(docs_df)):
+        #     print(f"{docs_df[docs_df['Topic']==int(t_num)]['datetime'].iloc[idx]}")
+        #     print(f"{docs_df[docs_df['Topic']==int(t_num)]['summary'].iloc[idx]}\n")
 
     else : break
--- model/summary/code/make_summary_dataframe.py
+++ model/summary/code/make_summary_dataframe.py
@@ -1,5 +1,5 @@
 import os
-from tqdm import tqdm
+from tqdm import tqdm, trange
 import pandas as pd
 import torch
 from torch.utils.data import TensorDataset, DataLoader
@@ -9,6 +9,8 @@ def make_summary_data(model, tokenizer, dataset_path, device):
     name_end = 'RY.csv'
     model.to(device)
     batch_size = 64
+    min_length = 30
+    max_length = 256
     model.eval()
 
     for file in file_list:
@@ -21,8 +23,25 @@ def make_summary_data(model, tokenizer, dataset_path, device):
         if 'summary' in total_data.columns:
             total_data = total_data.drop(columns=['summary'], axis=1)   #데이터 받고 요약 있으면 없애주기
         
-        input_ids = tokenizer(total_data['contents'].to_list(), return_tensors="pt", padding="max_length", truncation=True, max_length=1024)['input_ids']
-        input_dataset = TensorDataset(input_ids)
+        #input_ids = tokenizer(total_data['contents'].to_list(), return_tensors="pt", padding="max_length", truncation=True, max_length=1024)['input_ids']
+
+        input_ids = []
+        no_text_ids = []
+        for idx in trange(len(total_data), desc=f'tokenizing '):
+            text = total_data['contents'].iloc[idx]
+            text_len = len(tokenizer.tokenize(text))
+            
+            if text_len<min_length:
+                text = total_data['title'].iloc[idx] + '.' + text
+
+            tokens = tokenizer.encode(text, padding="max_length", truncation=True, max_length=1024)
+            input_ids.append(tokens)
+
+            if len(tokenizer.tokenize(text))<min_length:
+                no_text_ids.append(idx)
+
+        print(f'\n{no_text_ids=}\n')
+        input_dataset = TensorDataset(torch.tensor(input_ids))
         data_loader = DataLoader(input_dataset, batch_size=batch_size, shuffle=False)
 
         s_list = []
@@ -33,8 +52,8 @@ def make_summary_data(model, tokenizer, dataset_path, device):
                                         bos_token_id=model.config.bos_token_id,
                                         eos_token_id=model.config.eos_token_id,
                                         length_penalty=2.0,
-                                        max_length=256,
-                                        min_length=30,
+                                        max_length=max_length,
+                                        min_length=min_length,
                                         num_beams=6,
                                         repetition_penalty=2.0,
                                         ).to('cpu')
@@ -43,10 +62,13 @@ def make_summary_data(model, tokenizer, dataset_path, device):
             for ids in summary_text_ids:
                 output = tokenizer.decode(ids, skip_special_tokens=True)
                 s_list.append(output)  
+        
+        for idx in no_text_ids:
+            s_list[idx] = total_data['title'].iloc[idx]
 
         df_sum = pd.DataFrame({'summary' : s_list})
-        total_data = pd.concat([total_data, df_sum], axis=1)
 
+        total_data = pd.concat([total_data, df_sum], axis=1)
         total_data.to_csv(os.path.join("../../embedding/dataset", f'Sum_{file}'), index=False)
         print('\nAdd summary data finish!\n')
             
