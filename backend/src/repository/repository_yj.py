e1080b2e895a19cda5f226d4cd6f5ac5ae229f2d
Becky_kwon <59564626+Becky-Kwon@users.noreply.github.com>
2024-03-31 12:49:11 +0900

    Delete backend/src/repository/repository_yj.py

--- backend/src/repository/repository_yj.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Session
-from fastapi import Depends
-from database.connection import get_db
-
-class repository_yj:
-    def __init__(self, session: Session = Depends(get_db)):
-        self.session = session
-        
